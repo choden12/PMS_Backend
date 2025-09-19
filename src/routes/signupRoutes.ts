8adbc7398d55e20136ad46ce4bc1b6f88d7e0218
Sangay Choden <02240355.cst@rub.edu.bt>
2025-09-19 08:43:07 +0600

    main

--- src/routes/signupRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signupUser } from "../controllers/signupController";
-
-const router = Router();
-
-// POST /api/signup
-router.post("/", signupUser);
-
-export default router;
